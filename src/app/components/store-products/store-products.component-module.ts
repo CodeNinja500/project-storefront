25fa3a1312c22867aaa6e1500615bf409407fe12
Maciej Paluch <maciekpal@gmail.com>
2023-01-20 13:38:33 +0100

    Feat: Store details on store products page
    Store details and search box added.
    Search has no functionality yet.

--- src/app/components/store-products/store-products.component-module.ts
+++ src/app/components/store-products/store-products.component-module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StoreProductsComponent } from './store-products.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   declarations: [StoreProductsComponent],
   providers: [],
   exports: [StoreProductsComponent]
 })
-export class StoreProductsComponentModule {
-}
+export class StoreProductsComponentModule {}
