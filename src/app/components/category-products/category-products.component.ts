534d6e42ad73cb88e1b3e762085df130b7e76299
Maciej Paluch <maciekpal@gmail.com>
2023-01-21 14:19:40 +0100

    Feat: Category navbar and category details card created

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { CategoryModel } from '../../models/category.model';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-category-products',
@@ -8,4 +13,14 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryProductsComponent {
+  readonly categoryList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly categoryId$: Observable<string> = this._activatedRoute.params.pipe(
+    map((params) => params['categoryId']),
+    shareReplay(1)
+  );
+  readonly categoryDetails$: Observable<CategoryModel> = this.categoryId$.pipe(
+    switchMap((categoryId) => this._categoriesService.getSingleCategoryById(categoryId))
+  );
+
+  constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute) {}
 }
