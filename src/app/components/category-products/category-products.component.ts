a4a634c664ee29147bbcc8f32eaae203c229b992
Maciej Paluch <maciekpal@gmail.com>
2023-01-21 15:43:07 +0100

    Feat: product card updated with more details on (category-products page)

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { ProductQueryModel } from 'src/app/query-models/product.query-model';
 import { CategoryModel } from '../../models/category.model';
 import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
@@ -23,11 +24,17 @@ export class CategoryProductsComponent {
   readonly categoryDetails$: Observable<CategoryModel> = this.categoryId$.pipe(
     switchMap((categoryId) => this._categoriesService.getSingleCategoryById(categoryId))
   );
-  readonly productInCategoryList$: Observable<ProductModel[]> = this.categoryId$.pipe(
+  readonly productInCategoryList$: Observable<ProductQueryModel[]> = this.categoryId$.pipe(
     switchMap((categoryId) =>
       this._productsService
         .getAllProducts()
-        .pipe(map((products) => products.filter((product) => product.categoryId === categoryId)))
+        .pipe(
+          map((products) =>
+            products
+              .filter((product) => product.categoryId === categoryId)
+              .map((product) => this.mapToProductQueryModel(product))
+          )
+        )
     )
   );
 
@@ -36,4 +43,35 @@ export class CategoryProductsComponent {
     private _activatedRoute: ActivatedRoute,
     private _productsService: ProductsService
   ) {}
+
+  mapToProductQueryModel(product: ProductModel): ProductQueryModel {
+    return {
+      id: product.id,
+      categoryId: product.categoryId,
+      featureValue: product.featureValue,
+      imageUrl: product.imageUrl,
+      name: product.name,
+      price: product.price,
+      ratingCount: product.ratingCount,
+      ratingValue: product.ratingValue,
+      storeIds: product.storeIds,
+      ratingStars: this.mapRatingNumberToStars(product.ratingValue)
+    };
+  }
+
+  mapRatingNumberToStars(rating: number): number[] {
+    const ratingRounded = Math.round(rating * 2) / 2;
+    const starsArray = Array.from(Array(5).keys()).map((n) => {
+      if (n < Math.floor(ratingRounded)) {
+        return 1;
+      }
+      if (n == Math.floor(ratingRounded)) {
+        if (ratingRounded - Math.floor(ratingRounded) >= 0.5) return 0.5;
+        else return 0;
+      } else {
+        return 0;
+      }
+    });
+    return starsArray;
+  }
 }
