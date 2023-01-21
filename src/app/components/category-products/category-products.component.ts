1618ba4cc79c1c8324ccf69c401518fe70a6045b
Maciej Paluch <maciekpal@gmail.com>
2023-01-21 14:29:10 +0100

    feat: Product list created on category-products

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay, switchMap } from 'rxjs/operators';
 import { CategoryModel } from '../../models/category.model';
+import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-category-products',
@@ -21,6 +23,17 @@ export class CategoryProductsComponent {
   readonly categoryDetails$: Observable<CategoryModel> = this.categoryId$.pipe(
     switchMap((categoryId) => this._categoriesService.getSingleCategoryById(categoryId))
   );
+  readonly productInCategoryList$: Observable<ProductModel[]> = this.categoryId$.pipe(
+    switchMap((categoryId) =>
+      this._productsService
+        .getAllProducts()
+        .pipe(map((products) => products.filter((product) => product.categoryId === categoryId)))
+    )
+  );
 
-  constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute) {}
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _activatedRoute: ActivatedRoute,
+    private _productsService: ProductsService
+  ) {}
 }
