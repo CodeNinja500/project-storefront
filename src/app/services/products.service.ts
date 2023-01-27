07691a86b67541925c34971a0ea29ebbecc73a5f
Maciej Paluch <maciekpal@gmail.com>
2023-01-27 12:59:35 +0100

    moved repeatable function to service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -10,4 +10,20 @@ export class ProductsService {
   getAllProducts(): Observable<ProductModel[]> {
     return this._httpClient.get<ProductModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-products');
   }
+
+  mapRatingNumberToStars(rating: number): number[] {
+    const ratingRounded = Math.round(rating * 2) / 2;
+    const starsArray = Array.from(Array(5).keys()).map((n) => {
+      if (n < Math.floor(ratingRounded)) {
+        return 1;
+      }
+      if (n == Math.floor(ratingRounded)) {
+        if (ratingRounded - Math.floor(ratingRounded) >= 0.5) return 0.5;
+        else return 0;
+      } else {
+        return 0;
+      }
+    });
+    return starsArray;
+  }
 }
